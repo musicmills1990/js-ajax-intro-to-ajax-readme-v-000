e6abef097e845a8bf69257df3a7a19b364bf9344
Marc Mills <mdmills1990@gmail.com>
2019-03-07 16:02:54 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -28,14 +28,15 @@ function getCommits(el){
 
 function showCommits(){
   const commits = JSON.parse(this.responseText);
-  const commitsList = `<ul>${commits.map(
-    commit =>
-    '<li><strong>' +
-    commit.author.login +
-    '</strong> - ' +
-    commit.commit.message +
-    '</li>'
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        '<li><strong>' +
+        commit.author.login +
+        '</strong> - ' +
+        commit.commit.message +
+        '</li>'
   )
-  .join('')}</ul>`
+  .join('')}</ul>`;
   document.getElementById('commits').innerHTML = commitsList
 }
