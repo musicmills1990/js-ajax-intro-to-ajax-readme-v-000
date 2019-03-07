cb900399a843a464b70ccae1b44ca5dfb646a839
Marc Mills <mdmills1990@gmail.com>
2019-03-07 15:42:53 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
-// your code here
+function showRepositories(){
+  console.log(this.responseText);
+}
+
 function getRepositories(){
   const req = new XMLHttpRequest();
+  req.addEventListener('load', showRepositories);
   req.open('GET', 'https://api.github.com/users/octocat/repos');
   req.send();
 }
+
+function showRepositories(){
+  let repos = JSON.parse(this.responseText);
+  console.log(repos);
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`
+  document.getElementById('repositories').innerHTML = repoList;
+}
+
